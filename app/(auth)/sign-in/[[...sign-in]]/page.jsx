4a6ebb6b9ec07c8ef1855711fbc974bc8872b72b
Thakuraditya1512 <thakuradityasingh1512@gmail.com>
2024-05-07 11:02:26 +0530

    Commit this

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -64,12 +64,11 @@ export default function Page() {
           </a>
 
           <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-          Welcome to TubeGuruji 🦑
+          Welcome to Learning Destiny 🦑
           </h1>
 
           <p className="mt-4 leading-relaxed text-gray-500">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-            quibusdam aperiam voluptatum.
+          Aman Deepak Bhuwan
           </p>
         </div>
 
